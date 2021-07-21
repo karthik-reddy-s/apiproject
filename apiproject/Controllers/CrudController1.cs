fbdaa887fb0a59a9eb88eb71ea2657f95630fa3e
karthik <karthiksreddy10@gmail.com>
2021-07-21 14:25:40 +0530

    first commit

---
+++ apiproject/Controllers/CrudController1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        List<string> companies = new List<string>()
+        {
+            "facebook","netflix","apple","amazon","disney"
+        }; 
+        [HttpGet, Route("getAllCompanies")]
+        public IActionResult GetCompanies()
+        {
+          
+            var res = companies.Where(w => w.StartsWith('a')).ToArray();
+            if (res.Length > 0)
+            {
+                return Ok(res);
+            }
+            return NotFound(
+                "Datanotfound");
+        }
+        [HttpGet("{letter}"), Route("fromuser")]
+        public IActionResult GetFromUser([FromQuery] string letter)
+        {
+            try
+            {
+                var res = companies.Where(w => w.StartsWith(letter)).ToArray();
+                if (res.Length > 0)
+                {
+                    return Ok(res);
+                }
+                return NotFound(
+                    "Datanotfound");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+
+        }
+    }
+}
